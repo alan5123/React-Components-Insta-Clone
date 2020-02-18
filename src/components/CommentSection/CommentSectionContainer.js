98f2eaeb1c2e56607edb709e36262e2f08a86401
alan5123 <alanparker5123@gmail.com>
2020-02-18 16:31:59 -0500

    Searchbar, post, and comments done

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -4,12 +4,17 @@ import CommentInput from "./CommentInput";
 import Comment from "./Comment";
 import "./Comment.css";
 
+
 const CommentSection = props => {
   // Add state for the comments
-
+const [data] = useState(props.comments)
+console.log(data)
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
+      {data.map(c => (
+        <Comment comment = {c}/>
+      ))}
       <CommentInput />
     </div>
   );
